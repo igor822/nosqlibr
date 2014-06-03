f7bb238415c7b10877bd50bf620470e486cc5e2e
Igor Carvalho <igor.carvalho@work.srv.br>
2014-06-03 11:56:16 -0300

    Implemented method to insert into collection and methods to retriveve collection and database

--- lib/NoSqlibr/Driver/Mongo.php
+++ lib/NoSqlibr/Driver/Mongo.php
@@ -2,12 +2,23 @@
 namespace NoSqlibr\Driver;
 
 use NoSqlibr\Driver\DriverInterface;
+use NoSqlibr\Exception as NoSqlException;
 
+/**
+ * Driver to connect, read and write data to mongodb
+ * 
+ * @author Igor Carvalho <igor822@gmail.com>
+ * @since 2014-06-01
+ */
 class Mongo implements DriverInterface {
 
 	private $data = null;
 
-	private $driver = null;
+	protected $driver = null;
+
+	private $db = null;
+
+	private $collection = null;
 
 	public function __construct($data) {
 		$this->data = $data;
@@ -22,8 +33,24 @@ class Mongo implements DriverInterface {
 		return 'mongodb://'.$this->data['user'].':'.$this->data['pass'].'@'.$this->data['host'].':'.$this->data['port'];
 	}
 	
-	public function insert($data){
+	/**
+	 * Method to insert values to collection
+	 * 
+	 * @param $data Array of data with keys and values to add to collection
+	 * @param $options Array of options to insert values
+	 * @access public
+	 * @throws NoSqlException\CollectionException
+	 * @throws \Exception
+	 */
+	public function insert($data, $options = array()){
+		if (empty($this->collection)) throw new NoSqlException\CollectionException('Please select an collection to insert your values');
 
+		if (is_array($data)) {
+			$rs = $this->getCollection()->insert($data, $options);
+		} else {
+			throw new \Exception('Values must be an array');
+		}
+		return $rs;
 	}
 
 	public function update($data, $condition = array()){
@@ -35,11 +62,45 @@ class Mongo implements DriverInterface {
 	}
 
 	public function find($condition = array()){
-
+		return $this->collection->find($condition);
 	}
 
-	public function getDriver() {
-		return $this->driver;
+	public function selectDB($name) {
+		if (empty($this->driver)) $this->connect();
+		$this->db = $this->driver->selectDB($name);
+		return $this;
+	}
+
+	public function getConnections() {
+		if (!empty($this->driver)) return $this->driver->getConnections();
+	}
+
+	public function getDB() {
+		if (!empty($this->db)) return $this->db;
+		else throw new \Exception('DB is not selected');
+	}
+
+	public function listCollections($incl = false) {
+		return $this->getDB()->listCollections($incl);
+	}
+
+	public function selectCollection($name) {
+		if (!empty($this->collection)) return $this->collection;
+		return $this->collection = $this->getDB()->selectCollection($name);
+	}
+
+	public function createCollection($name, $options = array()) {
+		if (!in_array($name, $this->getDB()->getCollectionNames())) {
+			$defaultOptions = array('capped' => false, 'size' => '10*1024', 'max' => 10);
+			$this->collection = $this->getDB()->createCollection($name, array_merge($defaultOptions, $options));
+		} else {
+			$this->selectCollection($name);
+		}
+		return $this;
+	}
+
+	public function getCollection() {
+		return $this->collection;
 	}
 
 }
